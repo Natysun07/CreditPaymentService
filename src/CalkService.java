30a323007cc864fa2389efaf4f461171b4b7ebdf
Natalia <natysun07@rambler.ru>
2023-04-13 20:11:16 +0300

    Final commit

--- src/CalkService.java
+++ src/CalkService.java
@@ -2,10 +2,9 @@ public class CalkService {
     public double calculate(int term, int amountCredit) {
         double percent = 9.99;
         double monthPercent;
-        int month = 12;
         double degree;
         double amount;
-        monthPercent = percent / month / 100;
+        monthPercent = percent / 12 / 100;
         amount = (1 + monthPercent);
         degree = Math.pow(amount, term);
 
